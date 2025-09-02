92c2c59ee319868d22abb7e80a3a41d28ff8ff4a
Andres <andressanchez1816@gmail.com>
2025-09-02 15:30:19 -0300

    Modificacion del squema del producto por una version mas reciente y agregando compatibilidades con Pydantic

--- Backend/app/db/squemas/products_squemas.py
+++ Backend/app/db/squemas/products_squemas.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
-class Product(BaseModel):
-    nombre : str
-    sku : str
-    descripcion : str | None = None
-    stock : int
-    stock_minimo : int
+class ProductoCreate(BaseModel):
+    nombre: str
+    sku: str
+    stock: int = 0
+    stock_minimo: int = 0
+    descripcion: str | None = None   #Atributo opcional, por defecto esta vacio
 
+class ProductoResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  # permite mapear desde ORM (Lo que permite transformar de modelo ORM o modelo dominio a un JSON, que es lo que tiene que devolver un ENDPOINT)
+    id: int
+    nombre: str
+    sku: str
+    stock: int
+    stock_minimo: int
+    descripcion: str | None
