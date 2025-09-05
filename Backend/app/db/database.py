16603bd241bcca64f8014f1e332879563da10843
Andres <andressanchez1816@gmail.com>
2025-09-05 00:18:47 -0300

    Migracion del modelo ORM de producto con alembic a DB SQLITE3

--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -1,13 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # URL de la base de datos (SQLite en un archivo local llamado app.db)
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = "sqlite:///Backend/app/db/app.db"
 
 # Conexión al motor de SQLAlchemy
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# inspector = inspect(engine)
+# print(inspector.get_table_names())
 
 # Sesiones para interactuar con la BD
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- Backend/app/db/models_ORM/product_orm.py
+++ Backend/app/db/models_ORM/product_orm.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import  Mapped, mapped_column
-from app.database import Base
+from app.db.database import Base
 from sqlalchemy import Text, Integer, String
 
 class ProductoORM(Base):
     __tablename__ = "productos"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    nombre: Mapped[str] = mapped_column(String(100), nullable=False)
+    nombre: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     sku: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     stock: Mapped[int] = mapped_column(Integer, default=0)
     stock_minimo: Mapped[int] = mapped_column(Integer, default=0)
